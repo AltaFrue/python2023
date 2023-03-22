9c1cdb089f6a337b051c1aae5812024664b1b5e6
Altafrue <fkrgns56@gmail.com>
2023-03-22 18:35:02 +0900

    123

--- celfah.py
+++ celfah.py
@@ -1,8 +1,17 @@
-print("섭씨온도를 화씨온도로 변환하는 프로그랩입니다.")
-print("변환할 섭씨온도를 입력하세요")
+paju = 18.6
+seoul = 20.4
+deajun = 21.7
+deagu = 20.8
+busan = 16.6
 
-cel =  input()
-fah = (float(cel) * 1.8) +32
+print("파주시의 섭씨온도 : ", paju)
+print("서울시의 섭씨온도 : ", seoul)
+print("대전시의 섭씨온도 : ", deajun)
+print("대구시의 섭씨온도 : ", deagu)
+print("부산시의 섭씨온도 : ", busan)
 
-print("섭씨온도 : ", cel)
-print("화씨온도 : ", fah)
+print("파주시의 화씨온도 : ", (float(paju) * 1.8) +32)
+print("서울시의 화씨온도 : ", (float(seoul) * 1.8) +32)
+print("대전시의 화씨온도 : ", (float(deajun) * 1.8) +32)
+print("대구시의 화씨온도 : ", (float(deagu) * 1.8) +32)
+print("부산시의 화씨온도 : ", (float(busan) * 1.8) +32)
