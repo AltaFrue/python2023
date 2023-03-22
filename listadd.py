1a4750c37b1efd4707753b238714f18573ec31de
Altafrue <fkrgns56@gmail.com>
2023-03-22 19:02:53 +0900

    color

---
+++ listadd.py
@@ -0,0 +1,3 @@
+color1 = ['red', 'blue', 'green']
+color2 = ['orange', 'black', 'white']
+print(color1 + color2)
--- slicing.py
+++ slicing.py
@@ -1,2 +1,4 @@
 cities = ['서울', '부산', '인천', '대구', '대전', '광주', '울산', '수원']
 print(cities[0:6])
+print(cities[::2])
+print(cities[::-1])
