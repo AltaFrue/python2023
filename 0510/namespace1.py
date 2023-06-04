1dbf8605ed3389fbab468d4fc1d49758b48b29c2
AltaFrue <fkrgns56@gmail.com>
2023-06-04 13:55:46 +0900

    123

---
+++ 0510/namespace1.py
@@ -0,0 +1,3 @@
+import fah_converter as fah
+
+print(fah.covert_c_to_f(41.6))
---
+++ 0510/reference1.py
@@ -0,0 +1,5 @@
+__all__ = ["analysis", "crawling", "database"]
+
+from roboadvisor import analysis
+from roboadvisor import crawling
+from roboadvisor import database
